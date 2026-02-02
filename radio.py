fce106fe1a90efffa4cd11c2f453dd9396d9bf55
Vanja Andjelic <vanja.andelic@student.tugraz.at>
2026-02-02 05:28:44 +0100

    revamped graphing

--- radio.py
+++ radio.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 avogadros_constant = 6.022e23
+euler = 2.71828
 
 with open('isotopes.json', 'r') as file: 
     data = json.load(file)
@@ -29,7 +30,7 @@ def print_isotopes(data):
         print(f"{isotope}, {name}")
 
 
-#do smth so that it maybe prints out the element in a nice way
+#do smth so that it maybe prints out the element in a nice way but ill think ab it (GUI??? idk)
 def print_isotope_info(isotope):
     if isotope in data: 
         info = data[isotope]
@@ -71,7 +72,7 @@ def get_inputs(isotope):
     return mols, time, grams
 
 
-def simulate_decay(isotope, total_time, moles, dt = 1): 
+def simulate_decay_montecarlo(isotope, total_time, moles, dt = 1): 
     if isotope in data: 
         info = data[isotope]
 
@@ -94,10 +95,10 @@ def simulate_decay(isotope, total_time, moles, dt = 1):
         decayed = 0
 
 
-        # Fast Monte Carlo: binomial sampling
+        # fast montecarlo: binomial sampling, for loops break my laptop apparently - sample size too big
         decayed = np.random.binomial(num_atoms, p)
 
-        #for i in range(num_atoms):
+        #for i in range(num_atoms): 
              #if random.random() < p:
                 #decayed += 1 
 
@@ -109,21 +110,61 @@ def simulate_decay(isotope, total_time, moles, dt = 1):
 
     return times, counts
 
+def theoretical_decay(isotope, moles, total_time, dt=1): 
+    if isotope in data: 
+        info = data[isotope]
 
-def plot_decay(isotope, times, counts):
+    total_num_atoms = int(moles * avogadros_constant)
+    half_life = to_seconds(info["half_life"], info["unit"])
+    decay_const = math.log(2) / half_life
+
+    undecayed_num = []
+    times = []
+    i =0
+    while i<= total_time: 
+        undecayed = total_num_atoms * math.exp(-decay_const * i)
+        if(i == 0): 
+            undecayed_num.append(total_num_atoms)
+        else: 
+            undecayed_num.append(undecayed)
+
+        times.append(i)
+        i += dt
+    
+
+    return times, undecayed_num
+
+
+def plot_decay_montecarlo(isotope, times, counts):
 
     plt.xlabel("Time (seconds)")
     plt.ylabel("Number of atoms")
-    plt.title(f"Radioactive Decay of {isotope}")
+    plt.title(f"Radioactive Decay of {isotope} - Monte Carlo")
+    plt.plot(times, counts, color = "blue", label="Monte Carlo")
+    plt.show()
 
+def plot_decay_theoretical(isotope, theoretical_times, theoretical_counts):
 
-    plt.plot(times, counts)
+    plt.xlabel("Time (seconds)")
+    plt.ylabel("Number of atoms")
+    plt.title(f"Radioactive Decay of {isotope} - Theoretical")
+    plt.plot(theoretical_times, theoretical_counts, color = "orange", label = "Theoretical" )
+    plt.show()
+
+def plot_theory_montecarlo(isotope,times,counts, theoretical_times, theoretical_counts ): 
+    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+    axes[0].set_xlabel("Time (seconds)")
+    axes[1].set_xlabel("Time (seconds)")
+    axes[0].set_ylabel("Number of atoms")
+    axes[1].set_ylabel("Number of atoms")
+    fig.suptitle(f"Radioactive Decay of {isotope}: Theory vs Monte Carlo", fontsize=14)
+    
+
+    axes[0].plot(times, counts, color = "blue", label="Monte Carlo")
+    axes[1].plot(theoretical_times, theoretical_counts, color = "orange", label = "Theoretical" )
     plt.show()
 
     
-
-
-
 
 print(" ")
 print("======= Choose an isotope to see the decay for ========")
@@ -136,7 +177,11 @@ mols, time, grams = get_inputs(isotope)
 
 times = []
 counts = []
-times, counts = simulate_decay(isotope, time, mols, 1)
+times, counts = simulate_decay_montecarlo(isotope, time, mols, 1)
+theoretical_times, theoretical_counts = theoretical_decay(isotope, mols, time, 1)
 
-plot_decay(isotope, times, counts)
+plot_decay_montecarlo(isotope, times, counts)
+plot_decay_theoretical(isotope, theoretical_times, theoretical_counts)
+plot_theory_montecarlo(isotope, times, counts, theoretical_times, theoretical_counts)
 
+#todo: add chains, maybe do smth w the type of decay?? add equations perhaps idk ill see 
